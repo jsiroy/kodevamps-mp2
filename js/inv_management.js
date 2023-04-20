ec3bffe6e1ba4b8037017994f5b3d8a5bc50a5dc
IchadB <richardbetalmos@gmail.com>
2023-04-20 14:07:52 +0800

    Inventory management dummy data and JS config

---
+++ js/inv_management.js
@@ -0,0 +1,9 @@
+
+// Datatable loader script in Inventory Management - Richard
+// loader scripts starts here
+$(document).ready(function () {
+    $('#inventoryTable').DataTable({
+        pagingType: 'full_numbers',
+    });
+});
+// loader scripts ends here
